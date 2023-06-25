72c3758069afcbb9ac671f5a84a80e477d31da0f
“Nicholas” <danielnick210@gmail.com>
2023-06-25 08:35:56 -0700

    debug

--- _printf.c
+++ _printf.c
@@ -6,37 +6,50 @@
  * Return: number of args passed
  */
 
-
 int _printf(const char *format, ...)
 {
-	int unsigned i;
-	int r_value = r_val = 0;
-	va_list args;
-	va_start(args, format);
+    unsigned int i;
+    int r_value = 0;
+    int r_val = 0;
+    va_list args;
 
-	for(i = 0; format[i] != '\0'; i++)
-	{
-		if(format != '%')
-		{
-			print_out(format[i]);
-		}
-		else(format[h + 1] == 'c')
-		{
-			print_out(va_args(args, int));
-			i++;
-		}
-		else(format[h + 1] == 's')
-		{
-			r_val = print_string(va_args(args, char *));
-			i++;
-			r_value += (r_val - 1);
-		}
-		else(format[h + 1] == '%')
-		{
-			print_out('%');
-			i++;
-		}
-		r_value += 1;
-	}
-	 return(r_value);
+    va_start(args, format);
+    if (format)
+    {
+        for (i = 0; format[i] != '\0'; i++)
+        {
+            if (format[i] != '%')
+            {
+                print_out(format[i]);
+                r_value++;
+            }
+            else if (format[i + 1] == 'c')
+            {
+                print_out(va_arg(args, int));
+                i++;
+                r_value++;
+            }
+            else if (format[i + 1] == 's')
+            {
+                r_val = print_string(va_arg(args, char *));
+                i++;
+                r_value += r_val;
+            }
+            else if (format[i + 1] == '%')
+            {
+                print_out('%');
+                i++;
+                r_value++;
+            }
+        }
+    }
+    else
+    {
+        va_end(args);
+        return (-1);
+    }
+    va_end(args);
+
+    return (r_value);
 }
+
