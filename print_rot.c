03c007defd940b92dec4dedaec1e4c8e1d8e872c
“Nicholas” <danielnick210@gmail.com>
2023-06-29 11:25:03 +0100

    rot function

---
+++ print_rot.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * print_R - prints a string with rot13 encryption.
+ * @args: string to print.
+ * Return: number of chars printed.
+ */
+int print_R(va_list args)
+{
+	int i, j, r_value = 0;
+	char *str = va_arg(args, char *);
+	char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; str[i]; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (str[i] == alpha[j])
+			break;
+		}
+		if (str[i] == alpha[j])
+		{
+			_putchar(rot13[j]);
+			r_value+;
+		}
+		else
+		{
+			_putchar(str[i]);
+			r_value++;
+		}
+	}
+
+	return (r_value);
+}
